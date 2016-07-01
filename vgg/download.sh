126869f639ee197c562dbced328a8c6ff5b9ea44
phunterlau <phunter.lau@gmail.com>
2016-07-01 10:02:55 -0700

    add mkdir

--- vgg/download.sh
+++ vgg/download.sh
@@ -1,3 +1,4 @@
+mkdir ./model/
 wget https://dl.dropboxusercontent.com/u/10652630/kaggle/VGG_FC_ILSVRC_16_layers-symbol.json
 mv VGG_FC_ILSVRC_16_layers-symbol.json ./model/
 wget https://dl.dropboxusercontent.com/u/10652630/kaggle/VGG_FC_ILSVRC_16_layers-0074.params
